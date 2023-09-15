e5c8e24946664c63137792042f06e8268923a188
TrudyB-T <drtrudybannerman@gmail.com>
2023-09-15 07:18:38 -0700

    amenities

---
+++ models/amenity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" class Amenity """
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+        inherits from BaseModel
+
+        Attributes:
+                    name (str): string - empty string
+    """
+
+    name = ""
