d57634c148b231ef34f432417581bed7f6dd2ff0
Dedeyd70 <dorothyokoto@gmail.com>
2023-09-16 06:54:27 -0700

    updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """ class Filestorage"""
 import json
-import models
+from models.base_model import BaseModel
+from models.state import State
+from models.user import User
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
+from models.city import City
 
 
 class FileStorage:
